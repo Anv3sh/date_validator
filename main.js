30b90f656f430a31233ba89381b70f0ac0132df8
= <anveshgreat11@gmail.com>
2021-06-12 18:27:58 +0530

    indentation fix

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 function validate(dd,mm,yyyy){
-    var x= parseInt(dd.value);
-    var y= parseInt(mm.value);
-    var z= parseInt(yyyy.value);
+  var x= parseInt(dd.value);
+  var y= parseInt(mm.value);
+  var z= parseInt(yyyy.value);
 
-    var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+  var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
 // Non-leap year case:
 
-    if (y==1 || y>2 && y<=12)
+  if (y==1 || y>2 && y<=12)
   {
-  if (x>ListofDays[y-1])
-  {
-        alert('Invalid date format!');
-        return false;
-  }
-  else
-  {
-    alert('Valid date format!');
-    return false;
-  }
+    if (x>ListofDays[y-1])
+    {
+      alert('Invalid date format!');
+      return false;
+    }
+    else
+    {
+      alert('Valid date format!');
+      return false;
+    }
   }
 
 // Leap year case:
@@ -27,30 +27,29 @@ function validate(dd,mm,yyyy){
     var lyear = false;
     if ( (!(z % 4) && (z % 100)) || !(z % 400)) 
     {
-        lyear = true;
+      lyear = true;
     }
     //console.log(lyear);
     if ((lyear==false) && (x>=29))
     {
-        alert('Invalid date format!');
-        return false;
+      alert('Invalid date format!');
+      return false;
     }
     if ((lyear==true) && (x>29))
     {
-        alert('Invalid date format!');
-        return false;
+      alert('Invalid date format!');
+      return false;
     }
     if (((lyear==false) && (x<=28))||((lyear==true) && (x<=29)))
     {
       alert('Valid date format!');
       return false;
-      
     }
     
   }
   else
   {
-  alert("Invalid date format!");
-  return false;
+    alert("Invalid date format!");
+    return false;
   }
 }
