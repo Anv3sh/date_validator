07be3a011f4c10c0b6b06e8157c0dd6292bb8fce
= <anveshgreat11@gmail.com>
2021-06-12 10:00:28 +0530

    added validate function in main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function validate(dd,mm,yyyy){
+    var x= parseInt(dd.value);
+    var y= parseInt(mm.value);
+    var z= parseInt(yyyy.value);
+
+    var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+}
